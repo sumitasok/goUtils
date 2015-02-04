016b1f10edcd28a6bfb4a927e01c88bfcae03598
Sumit Asok <sumitasok@gmail.com>
2015-02-05 00:11:53 +0530

    req and resp:

--- http.go
+++ http.go
@@ -6,7 +6,19 @@ import (
 	"net/http"
 )
 
-func reqJsonToObject(req *http.Request, v interface{}) error {
+func ReqJsonToObject(req *http.Request, v interface{}) error {
+	defer req.Body.Close()
+
+	content, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		return err
+	} else {
+		json.Unmarshal(content, &v)
+		return nil
+	}
+}
+
+func RespJsonToObject(req *http.Response, v interface{}) error {
 	defer req.Body.Close()
 
 	content, err := ioutil.ReadAll(req.Body)
