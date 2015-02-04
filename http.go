82cc0fc76a13aaff4b02743c9cb02ff3a304cd18
Sumit Asok <sumitasok@gmail.com>
2015-02-05 00:28:45 +0530

    Post Json

--- http.go
+++ http.go
@@ -1,6 +1,7 @@
 package goUtils
 
 import (
+	"bytes"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
@@ -28,4 +29,21 @@ func RespJsonToObject(req *http.Response, v interface{}) error {
 		json.Unmarshal(content, &v)
 		return nil
 	}
+}
+
+func PostJson(url string, params interface{}) []byte {
+	jsonStr, _ := json.Marshal(params)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	content, _ := ioutil.ReadAll(resp.Body)
+
+	return content
 }
